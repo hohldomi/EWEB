bc872dfceaba7abb0b9db13a983ea121d7d266a2
Dominik Hohl <hohld4@bfh.ch>
2026-03-18 09:36:33 +0100

    Refactor toggle script for cleaner interaction

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,14 @@
 const button = document.getElementById("toggleButton");
 const text = document.getElementById("extraText");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
+    const isHidden = text.style.display === "" || text.style.display === "none";
 
-    if (text.style.display === "none") {
+    if (isHidden) {
         text.style.display = "block";
         button.textContent = "Weniger anzeigen";
     } else {
         text.style.display = "none";
         button.textContent = "Mehr anzeigen";
     }
-
 });
